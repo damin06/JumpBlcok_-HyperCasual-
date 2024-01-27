2c376e7db901a00fc5fb9dd499a72a2fe5e4ec6b
damin06 <ko060614@gmail.com>
2024-01-28 03:17:13 +0900

    jump effect

--- Assets/02_Scripts/Agent/AgentController.cs
+++ Assets/02_Scripts/Agent/AgentController.cs
@@ -3,22 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.EventSystems;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System;
 
 [RequireComponent(typeof(Rigidbody))]
 public class AgentController : MonoBehaviour
 {
-    private Rigidbody m_rb;
-    [SerializeField] private float m_jumpPower = 0f;
-    [SerializeField] private float m_jumpSpeed = 2f;
-    private float m_touch = 0f;
-
-    [Space]
-
     [Header("Baizer Test")]
     public Vector3 P1;
     public Vector3 P2;
     public Vector3 P3;
     public Vector3 P4;
+
+    [Space]
+
+    [Header("Agent Setting")]
+    [SerializeField] private float m_jumpSpeed = 2f;
+    [SerializeField] private float m_maxJumpPower = 5f;
+    [SerializeField] private AnimationCurve m_animationCurve;
+
+
+    private bool isGround = false;
+    private Rigidbody m_rb;
+    private float m_jumpPower;
+
+    public float JumpPower
+    {
+        get { return m_jumpPower; }
+        set { m_jumpPower = Mathf.Clamp(value, 0.1f, m_maxJumpPower); }
+    }
+
 
 
     private void Awake()
@@ -29,22 +43,24 @@ public class AgentController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        AgentInput();
     }
 
     private void AgentInput()
     {
-        if(Input.touchCount > 0)
+        if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
             if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
                 return;
 
-            m_jumpPower += Time.deltaTime;
+            JumpPower += Time.deltaTime;
 
-            if(touch.phase== TouchPhase.Ended)
+            if (touch.phase == TouchPhase.Ended)
             {
-
+                Vector3 dir = BlockManager.Instance.m_curDir == BlockDir.Right ? Vector3.right : Vector3.forward;
+                Jump(dir, JumpPower);
+                JumpPower = 0;
             }
         }
     }
@@ -52,11 +68,13 @@ public class AgentController : MonoBehaviour
     [ContextMenu("Jump Right")]
     public void JumpeTest()
     {
+        //m_rb.AddForce(new Vector3(1, 1f, 0) * 12, ForceMode.Impulse);
         StartCoroutine(Jump(Vector3.right, 10f));
     }
 
     private IEnumerator Jump(Vector3 dir, float distance)
     {
+        float time = 0f;
         float value = 0f;
         float curve = 5f;
 
@@ -65,15 +83,21 @@ public class AgentController : MonoBehaviour
         P3 = transform.position + (dir * distance) + (Vector3.up * curve);
         P4 = transform.position + (dir * distance);
 
-        while(value < 1f)
+        while (value < 1f)
         {
             transform.position = BazierCurve(P1, P2, P3, P4, value);
-            value = Mathf.Lerp(value, 1, Time.deltaTime);
-
+            //value = Mathf.Lerp(value, 1.1f, Time.deltaTime);
+            time = Mathf.Clamp(time + Time.deltaTime, 0, 1);
+            //value = m_animationCurve.Evaluate(time);
+            value = easeOutCirc(time);
+            Debug.Log(value);
             yield return null;
         }
-        Debug.Log("Break!");
+        Debug.Log("break");
     }
+
+    private float easeOutCirc(float value) => (Mathf.Sqrt(1 - Mathf.Pow(value-1, 2)));
+    private float easeOutQuad(float value) => (1 - (1 - value) * (1 - value));
 
     public Vector3 BazierCurve(Vector3 P1, Vector3 P2, Vector3 P3, Vector3 P4, float value)
     {
